ccf52a74d3f4b9fa1d81a3380fa157403bb3ee14
Oscar Plumariega <oscarplumariegacebo@gmail.com>
2024-11-23 15:17:08 +0100

    Generate PDF

--- src/app/core/services/common-service.service.ts
+++ src/app/core/services/common-service.service.ts
@@ -35,42 +35,42 @@ export class CommonService {
     }
 
     this.budgetService.getBudgetById(id).subscribe((budget:any) => {
-      this.userService.getUserById(budget.idBusiness).subscribe((user: any) => {
-        this.clientService.getClientById(budget.clientId).subscribe({
+      this.userService.getUserById(budget.IdBusiness).subscribe((user: any) => {
+        this.clientService.getClientById(budget.ClientId).subscribe({
           next: (client:any) =>{
             const doc = new jsPDF();
   
             doc.setFontSize(28);
             doc.setFont('courier');
             //title
-            doc.text(title + ' - ' + budget.name.split('-').pop(), 100, 20);
+            doc.text(title + ' - ' + budget.Name.split('-').pop(), 100, 20);
 
             if(user.logo != null){
-              doc.addImage(user.logo, 'JPEG', 0, 0, 30, 30);
+              doc.addImage(user.Logo, 'JPEG', 0, 0, 30, 30);
             }
     
             doc.setFontSize(14);
             //user data
-            doc.text(user.fullName, 10, 40);
-            doc.text(user.email, 10, 50);
-            doc.text(user.nif, 10, 60);
-            doc.text(user.address, 10, 70);
-            doc.text(user.region + ' ' + user.country, 10, 80);
-            doc.text(user.phoneNumber, 10, 90);
+            doc.text(user.FullName, 10, 40);
+            doc.text(user.Email, 10, 50);
+            doc.text(user.Nif, 10, 60);
+            doc.text(user.Address, 10, 70);
+            doc.text(user.Region + ' ' + user.Country, 10, 80);
+            doc.text(user.PhoneNumber.toString(), 10, 90);
     
             //client data
-            doc.text(client.name, 120, 40);
-            doc.text(client.email, 120, 50);
-            doc.text(client.nif, 120, 60);
-            doc.text(client.address, 120, 70);
-            doc.text(client.region + ' ' + client.country, 120, 80);
-            doc.text(client.phoneNumber, 120, 90);
+            doc.text(client.Name, 120, 40);
+            doc.text(client.Email, 120, 50);
+            doc.text(client.Nif, 120, 60);
+            doc.text(client.Address, 120, 70);
+            doc.text(client.Region + ' ' + client.Country, 120, 80);
+            doc.text(client.PhoneNumber.toString(), 120, 90);
 
             let bodyFormatItems = [];
 
-            const items = JSON.parse(budget.descriptionItems);
+            const items = JSON.parse(budget.DescriptionItems);
             for (let i = 0; i < items.length; i++) {
-              bodyFormatItems.push([items[i].name, items[i].units, items[i].price, items[i].totalConcept]);
+              bodyFormatItems.push([items[i].Name, items[i].Units, items[i].Price, items[i].TotalConcept]);
             }
 
             autoTable(doc, {
@@ -79,13 +79,13 @@ export class CommonService {
               body: bodyFormatItems,
             })
 
-            doc.text("Subtotal   " + budget.price, 150, 260);
-            doc.text("IVA 21%   " + Number((budget.price*0.21).toFixed(2)) + "€", 150, 270);
+            doc.text("Subtotal   " + budget.Price, 150, 260);
+            doc.text("IVA 21%   " + Number((budget.Price*0.21).toFixed(2)) + "€", 150, 270);
 
             doc.setFont('courier','bold');
-            doc.text("TOTAL   " + Number((budget.price*1.21).toFixed(2)) + "€", 150, 290);
+            doc.text("TOTAL   " + Number((budget.Price*1.21).toFixed(2)) + "€", 150, 290);
     
-            doc.save(title + '-' + budget.name.split('-').pop()+".pdf");
+            doc.save(title + '-' + budget.Name.split('-').pop()+".pdf");
           } 
         })
       });
