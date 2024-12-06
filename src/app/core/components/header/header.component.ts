74240551b98f3c751f4e1796b16661452ed5e700
Oscar Plumariega <oscarplumariegacebo@gmail.com>
2024-12-06 17:04:37 +0100

    RouterLink

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component} from '@angular/core';
+import { Router, RouterLink, RouterLinkActive, RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,RouterLink,RouterLinkWithHref, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  public href: string = "";
+
+  constructor(private router : Router) {}
 
   logout(){
     localStorage.clear();
+  }
+
+  ngOnInit() {
+    //this.href = this.router;
+    console.log(this.router.url);
+  }
+
+  activeNav(element: string){
+    //document.getElementById(element)!.classList.add('selected');
   }
 }
